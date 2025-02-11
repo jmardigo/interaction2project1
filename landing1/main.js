9eabb3658cf6fbf2a9274f1696cc74da6a9b38a5
jmardigo <diazj599@newschool.edu>
2025-02-10 19:33:54 -0500

    intro page starting

---
+++ landing1/main.js
@@ -0,0 +1,8 @@
+let navBar = document.querySelector(".nav-bar");
+let navExpanded = document.querySelector(".nav-expanded");
+
+
+navBar.addEventListener("mouseenter", navExpanded);
+navExpanded.addEventListener("mouseleave", navBar);
+
+//javascript is hard.

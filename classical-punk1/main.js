5453a948d1dfe0b7c19cd39d412a7dc12ab42434
jmardigo <diazj599@newschool.edu>
2025-02-12 01:11:12 -0500

    animated titles!!!
    I FIGURED IT OUT. I GOT IT TO WORK. THANK GOD.

---
+++ classical-punk1/main.js
@@ -0,0 +1,22 @@
+const chickenCase = str => str
+  .split('')
+  .map(l => Math.random() < 0.5 &&
+    l.toUpperCase() ||
+    l.toLowerCase()
+  )
+.join('')
+//honestly idk how the code above works but shoutout jakob on stackoverflow
+
+
+console.log(title) //debug
+
+
+function replaceTitle(){
+  let title = chickenCase("classical punk");
+  document.getElementById("title").innerHTML = title;
+  console.log(title)
+}
+
+
+//calls the function at intervals of 300ms
+setInterval(replaceTitle, 300)
